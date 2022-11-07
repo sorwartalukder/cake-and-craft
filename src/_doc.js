157bf070f59464862556e2c4a7d600af20dc44c6
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-07 21:55:18 +0600

    start work

---
+++ src/_doc.js
@@ -0,0 +1,17 @@
+/*
+https://preview.themeforest.net/item/roofix-roofing-services-wordpress-theme/full_screen_preview/27855848?_ga=2.232734879.1044135846.1667826000-1177760069.1658420376
+
+
+https://preview.themeforest.net/item/techbiz-it-solution-service-wordpress-theme/full_screen_preview/40006717?_ga=2.232734879.1044135846.1667826000-1177760069.1658420376
+
+
+https://preview.themeforest.net/item/taxseco-online-taxi-service-wordpress-theme/full_screen_preview/40241569?_ga=2.232734879.1044135846.1667826000-1177760069.1658420376
+
+*/
+
+/*
+client: https://github.com/Porgramming-Hero-web-course/b6a11-service-review-client-side-sorwartalukder
+
+server:  https://github.com/Porgramming-Hero-web-course/b6a11-service-review-server-side-sorwartalukder
+
+*/
