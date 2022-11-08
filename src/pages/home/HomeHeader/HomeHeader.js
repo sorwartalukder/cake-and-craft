972ee49f5c29468ecdfb328b9e333cc2529d41c1
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-08 12:26:52 +0600

    edit home banner

--- src/pages/home/HomeHeader/HomeHeader.js
+++ src/pages/home/HomeHeader/HomeHeader.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import './HomeHeader.css'
-import HomeCover from '../../../assets/images/header.png'
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
 
 const HomeHeader = () => {
-    const HomeCoverStyles = {
-        backgroundImage: `url(${HomeCover})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: '70vh',
 
-    };
     return (
-        <div >
-            <div className='relative'>
-                <div className='home-cover' style={HomeCoverStyles}>
-                    <div className='home-cover-text'>
-                        <h1 className=' text-5xl'>Made With Love</h1>
-                        <button className='btn bg-white text-black font-bold text-lg mt-4 px-8'>PURCHASE NOW <HiArrowNarrowRight className='ml-3 text-red-700' /></button>
-                    </div>
-
+        <div>
+            <div className='  bg-slate-900'>
+                <div className=' text-center font-bold text-white py-32 lg:py-60'>
+                    <h1 className='text-4xl lg:text-7xl '>Made With Love. <br /> I am ready. <br /> You ready?</h1>
+                    <button className='btn bg-white text-black font-bold text-lg mt-10 px-8'>PURCHASE NOW <HiArrowNarrowRight className='ml-3 text-red-700' /></button>
                 </div>
-
             </div>
         </div>
     );
