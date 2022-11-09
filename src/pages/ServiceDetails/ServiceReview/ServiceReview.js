c188dab1cc22085e26cd0f0a27393d622e802273
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-09 18:21:41 +0600

    send review data client to server

--- src/pages/ServiceDetails/ServiceReview/ServiceReview.js
+++ src/pages/ServiceDetails/ServiceReview/ServiceReview.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { HiStar } from 'react-icons/hi';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const ServiceReview = ({ service }) => {
+    const { user } = useContext(AuthContext)
     const [ratings, setRatings] = useState(0)
     console.log(service)
     const { _id, title, img, description, price, ingredient } = service;
@@ -10,7 +12,12 @@ const ServiceReview = ({ service }) => {
     const handleReviews = event => {
         event.preventDefault()
         const feedback = event.target.message.value;
-
+        const review = {
+            serviceId: _id,
+            userId: user.uid,
+            feedback,
+            ratings
+        }
     }
 
     return (
@@ -20,6 +27,16 @@ const ServiceReview = ({ service }) => {
             <div className=' mt-11' style={{ width: '250px' }}>
                 <form onSubmit={handleReviews}>
                     <h2 className="text-3xl font-semibold">Review</h2>
+
+                    <div className='flex items-center'>
+                        <div className="avatar online mt-4">
+                            <div className="w-11 rounded-full ring ring-primary">
+                                <img src={user?.photoURL} alt='' />
+                            </div>
+                        </div>
+                        <h5 className='text-xl font-semibold mt-3 ml-4'>{user.displayName}</h5>
+                    </div>
+
                     <div className='flex text-xl mt-4'>
                         <h6 className='mr-3'>Ratings: </h6>
                         <div className='text-3xl text-orange-500'>
